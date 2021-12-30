ae2952f04107d7c7cf6e22fe30082f661fc9aee9
Camilo Carrillo <ccamilocubillos@gmail.com>
2021-12-30 03:20:32 -0500

    Realziacion del reto de clase, cambio de orientacion y agregamos .gitignore

--- CANVAS/dibujo.js
+++ CANVAS/dibujo.js
@@ -2,10 +2,7 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d"); //del canvas vamos a obtener el lienzo
 var lineas = 30;
 var l = 0;
-var yi, xf ;
-
 //CREAMOS UNA FUNCION
-
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
 {
     lienzo.beginPath(); //inciia trazado
@@ -18,19 +15,17 @@ function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
 
 while(l < lineas)
 {
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea("green",0,yi,xf,300);
+    dibujarLinea("green",10 * l,0,300,10 * (l + 1));
     l ++;
 }
 
-dibujarLinea("black",1,1,1,299) //margen izquierdo
-dibujarLinea("black",1,299,299,299) // margen derecho
+dibujarLinea("black",1,1,299,1) //margen izquierdo
+dibujarLinea("black",299,1,299,299) // margen derecho
 
-for (l = 0;l < lineas;l++)
+/*for (l = 0;l < lineas;l++)
 {
     yi = 10 * l;
     xf = 10 * (l + 1);
     dibujarLinea("green",0,yi,xf,300);
     l ++;
-}
+}*/
