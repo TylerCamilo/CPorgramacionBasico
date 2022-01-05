2911f5990ee1a6d366504145f8a9c08957c8a0b4
Camilo Carrillo <ccamilocubillos@gmail.com>
2022-01-05 03:34:50 -0500

    CLASE 19, CON TAREAS PUESTAS CODIGO FUNCIONAL

--- VILLAPLATZI/platzi.js
+++ VILLAPLATZI/platzi.js
@@ -1,46 +1,63 @@
 var vp = document.getElementById("villa"); //conetxto de villaplatzi
 var papel = vp.getContext("2d"); //traer donde vamos a dibujar
-var mapa = "tile.png"; //fondo cargado en variable mapa
 
 //vamor a cargar la imagen dentro del canvas, crear un objeto nuevo
 //crear objeto de la nada en este caso
 // new Image(); la definicion un objeto con primer letra en Mayús
 //en image es como crear una etiqueta img de HTML
-var fondo = new Image(); // creamos una imagen creando un nuevo objeto
-fondo.src = mapa; //para poder cargar la fuente de la imagen hay que pasarla por el atributo src
-fondo.addEventListener("load", dibujar);
+var fondo = {
+    url:"tile.png",
+    cargaOK: false
+}
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false
+};
+//en js no hay fomra de agregarle  a un objeto new image a una de
+//de JSON
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarVacas);
+fondo.imagen = new Image(); // creamos una imagen creando un nuevo objeto
+fondo.imagen.src = fondo.url; //para poder cargar la fuente de la imagen hay que pasarla por el atributo src
+fondo.imagen.addEventListener("load", cargarFondo);
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarCerdos);
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
 
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarPollos);
+function cargarFondo()
+{
+    fondo.cargaOK = true;
+    dibujar();
+
+}
+
+function cargarVacas()
+{
+    vaca.cargaOK = true;
+    dibujar();
+}
 
 
 function dibujar()
 {
-    papel.drawImage(fondo, 0, 0);
-}
+    if(fondo.cargaOK)
+    {
+        papel.drawImage(fondo.imagen,0,0)
+    }
+    if(vaca.cargaOK)
+    {
+        var cantidad = aleatorio(5,25);
+        console.log(cantidad);
 
-function dibujarVacas()
-{
-    papel.drawImage(vaca,10, 10);
-}
+        for (var v = 0;v < cantidad; v++)
+        {
+            var x = aleatorio(0,420)
+	        var y = aleatorio (0,420)
+            papel.drawImage(vaca.imagen,x,y)
 
-function dibujarCerdos()
-{
-    papel.drawImage(cerdo,10, 300);
-}
-
-function dibujarPollos()
-{
-    papel.drawImage(pollo, 300, 150);
+        }
+        
+    }
 }
 
 function aleatorio (min,maxi)
