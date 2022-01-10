c00d12f80f239ce5e3193dcff4c5c392aa44959c
Camilo Carrillo <ccamilocubillos@gmail.com>
2022-01-10 04:52:24 -0500

    CLASE 19, Actualizacion de codigo, FUNCIONAL

--- VILLAPLATZI/platzi.js
+++ VILLAPLATZI/platzi.js
@@ -1,18 +1,19 @@
 var vp = document.getElementById("villa"); //conetxto de villaplatzi
 var papel = vp.getContext("2d"); //traer donde vamos a dibujar
 
-//vamor a cargar la imagen dentro del canvas, crear un objeto nuevo
+//vamor a cargar la imagen dentro del canvas,es decir crear un objeto nuevo
 //crear objeto de la nada en este caso
 // new Image(); la definicion un objeto con primer letra en Mayús
 //en image es como crear una etiqueta img de HTML
 var fondo = {
     url:"tile.png",
     cargaOK: false
-}
+};
 var vaca = {
     url: "vaca.png",
     cargaOK: false
 };
+
 //en js no hay fomra de agregarle  a un objeto new image a una de
 //de JSON
 
@@ -28,15 +29,19 @@ function cargarFondo()
 {
     fondo.cargaOK = true;
     dibujar();
-
 }
-
+ 
 function cargarVacas()
 {
     vaca.cargaOK = true;
     dibujar();
 }
 
+function cargarCerdos()
+{
+    cerdo.cargaOK = true;
+    dibujar();
+}
 
 function dibujar()
 {
@@ -44,15 +49,17 @@ function dibujar()
     {
         papel.drawImage(fondo.imagen,0,0)
     }
+
     if(vaca.cargaOK)
     {
         var cantidad = aleatorio(5,25);
         console.log(cantidad);
-
         for (var v = 0;v < cantidad; v++)
         {
-            var x = aleatorio(0,420)
-	        var y = aleatorio (0,420)
+            var x = aleatorio(0,7) //Posicion aceptable
+	        var y = aleatorio (0,7)
+            var x = x * 60;
+            var y = y * 60;
             papel.drawImage(vaca.imagen,x,y)
 
         }
