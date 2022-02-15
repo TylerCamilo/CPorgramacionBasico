d9197ec10d2b70446ddb125a382fe64f94a0679b
TylerCamilo1 <4ckamilo27@gmail.com>
2022-02-14 21:38:16 -0500

    Clase 23 actualización funcional

--- CAJERO/cajero.js
+++ CAJERO/cajero.js
@@ -9,6 +9,9 @@ class Billete
 
 function entregarDinero()
 {
+    
+    var t  =document.getElementById("dinero");
+    dinero = parseInt(t.value); // usamos parsear(castear) a int el entero de la cadena caracteres
     for (var bi of caja)
     {
         if (dinero > 0)
@@ -23,23 +26,40 @@ function entregarDinero()
                 papeles = div;
             }
             entregado.push( new Billete(bi.valor, papeles));
-            dinero = dinero - (bi.valor * papeles);           
-        }
-
+            dinero = dinero - (bi.valor * papeles);            
+        } 
     }
-    console.log(entregado);
-}
+    if (dinero > 0)
+    {        
+        resultado.innerHTML = ("No tengo ese dinero");
+    }
+    else
+    {
+        for(var e of entregado)
+        {
+            if(e.cantidad > 0)
+            {
+                resultado.innerHTML = resultado.innerHTML + e.cantidad + " billetes de  $" + e.valor + "<br/>";
+            }            
+        }
+    }
 
+    console.log(entregado);
+    
+}
 
 var caja = [];
 var entregado = [];
-caja.push(new Billete(50,3) ); //coloca 3 billetes de 50
-caja.push(new Billete(20,2) );
-caja.push(new Billete(10,2) );
+caja.push(new Billete(100,5) );
+caja.push(new Billete(50,10) ); //coloca 3 billetes de 50
+caja.push(new Billete(20,30) ); //coloca 2 billetes de 20
+caja.push(new Billete(10,10) ); //coloca 2 billetes de 10
+caja.push(new Billete(5,5) );
 
-var dinero = 210;
+var dinero = 0;
 var div = 0;
 var papeles = 0;
 
+var resultado = document.getElementById("resultado");
 var b = document.getElementById("extraer"); // obtener elemento de html von un id 
 b.addEventListener("click", entregarDinero); // cuando hace click dispara la funcion
